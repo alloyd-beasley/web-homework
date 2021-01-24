123a74917a10471f920680e1b59708842261ef02
Andrew Beasley <alloydbeasley@gmail.com>
2021-01-24 17:22:23 -0600

    edit and delete mutations

--- webserver/schema/mutations.js
+++ webserver/schema/mutations.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLFloat, GraphQLList } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLFloat } = graphql
 const { TransactionModel } = require('../data-models/Transaction')
-const { TransactionType, TransactionInputType } = require('./transaction-type')
+const { TransactionType } = require('./transaction-type')
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -24,6 +24,39 @@ const mutation = new GraphQLObjectType({
         tx.id = saved._id
 
         return tx
+      }
+    },
+    editTransactionById: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString },
+        user_id: { type: GraphQLString },
+        description: { type: GraphQLString },
+        merchant_id: { type: GraphQLString },
+        debit: { type: GraphQLBoolean },
+        credit: { type: GraphQLBoolean },
+        amount: { type: GraphQLFloat }
+      },
+      /* eslint-disable-next-line camelcase */
+      resolve: async (parentValue, { id, user_id, description, merchant_id, debit, credit, amount }) => {
+        const saved = await TransactionModel.findOneAndUpdate({ _id: id }, { user_id, description, merchant_id, debit, credit, amount }, {
+          new: true
+        })
+
+        return saved
+      }
+    },
+    removeTransactionById: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      /* eslint-disable-next-line camelcase */
+      resolve: async (parentValue, { id }) => {
+        const removed = await TransactionModel.findByIdAndDelete({ _id: id })
+        removed.id = removed._id
+
+        return removed
       }
     }
   }
